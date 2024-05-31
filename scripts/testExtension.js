1b94f7c4adfcb8419da754aeb37725493fc05f0e
Balint Lendvai <lendvai.balint@gmail.com>
2024-05-31 18:42:08 +0200

    Update testExtension.js

--- scripts/testExtension.js
+++ scripts/testExtension.js
@@ -1,6 +1,6 @@
 (function () {
 	function publish(payload) {
-		alert(`HELLO from the external extension script! ${JSON.stringify(payload, null, 2)}`);
+		alert(`HELLO from the external extension script! Payload: ${JSON.stringify(payload, null, 2)}`);
 	}
 
 	window.extensionTeamCenter = {
