93434d57aa9aea8d68cecfaf44581fe2734c0064
Balint Lendvai <lendvai.balint@gmail.com>
2024-06-11 14:42:46 +0200

    Update testExtension.js

--- scripts/testExtension.js
+++ scripts/testExtension.js
@@ -3,7 +3,21 @@
 		alert(`HELLO from the external extension script! Payload: ${JSON.stringify(payload, null, 2)}`);
 	}
 
+	// Function to mark an item for synchronization
+	function markToSync(element) {
+		// Here, you can add your logic to mark the item for synchronization
+		console.log(`Item ${element.data.elementId} marked for synchronization`);
+	}
+
+	// Function to unmark an item from synchronization
+	function unMarkToSync(element) {
+		// Here, you can add your logic to unmark the item from synchronization
+		console.log(`Item ${element.data.elementId} unmarked from synchronization`);
+	}
+
 	window.extensionTeamCenter = {
 		publish,
+		markToSync,
+		unMarkToSync,
 	};
 })();
