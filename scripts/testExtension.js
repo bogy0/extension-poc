d43b004ae6b728d59582790a976cb8ca42ddb386
Balint Lendvai <lendvai.balint@gmail.com>
2024-06-11 15:00:05 +0200

    Update testExtension.js

--- scripts/testExtension.js
+++ scripts/testExtension.js
@@ -6,13 +6,15 @@
 	// Function to mark an item for synchronization
 	function markToSync(element) {
 		// Here, you can add your logic to mark the item for synchronization
-		alert(`Item ${element.data.elementId} marked for synchronization`);
+		console.log({ element });
+		alert(`Item marked for synchronization`);
 	}
 
 	// Function to unmark an item from synchronization
 	function unMarkToSync(element) {
 		// Here, you can add your logic to unmark the item from synchronization
-		alert(`Item ${element.data.elementId} unmarked from synchronization`);
+		console.log({ element });
+		alert(`Item unmarked from synchronization`);
 	}
 
 	window.extensionTeamCenter = {
